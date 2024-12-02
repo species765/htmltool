50053e9fb8671b80eb17850f6a51ceedb3978563
species765 <clonemaid007@gmail.com>
2024-12-02 18:52:10 +0900

    aif.js の更新

--- js/aif.js
+++ js/aif.js
@@ -7,7 +7,7 @@ function formatDateToJapanese(dateInput) {
   const hours = date.getHours();
   const minutes = date.getMinutes();
 
-  const formattedDate = `${year}年${month}月${day}日 ${hours}時${minutes}分`;
+  const formattedDate = `${year}年${month}月${day}日　${hours}時${minutes}分`;
 
   return {
     formattedDate: formattedDate,
