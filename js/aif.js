843fe16334fce392cc313b52ed6d6cef77160ef9
species765 <clonemaid007@gmail.com>
2024-12-05 05:25:37 +0900

    aif.js の更新

--- js/aif.js
+++ js/aif.js
@@ -75,10 +75,92 @@ async function getHeadlinesNHK() {
       return { news: result };
     } else {
       return { news: "Failed to get news headlines" };
-      console.error('Failed to fetch HTML:', response.status);
+      //console.error('Failed to fetch HTML:', response.status);
     }
   } catch (error) {
     return { news: "Failed to get news headlines" };
-    console.error('Error fetching HTML:', error);
+    //console.error('Error fetching HTML:', error);
+  }
+}
+
+
+
+function stringJsObjToJson(input) {
+  let result = input
+    .replace(/(?<!\\)'(.*?)(?<!\\)'/g, (match, capturedContent) => {
+      const processedContent = capturedContent.replace(/([\/,:])/g, "\\$1");
+      return `'${processedContent}'`;
+    })
+    .replace(/[\t ]*\/\/.+\n/g, "")
+    .replace(/(\w+?):/g, "\"$1\":")
+    .replace(/(?<!\\),(?=\s*?[\}\]])/g, "")
+    .replace(/(?<!\\)'(.*?)(?<!\\)'/g, (match, capturedContent) => {
+      const processedContent = capturedContent.replace(/\\([\/,:])/g, "$1");
+      return `"${processedContent}"`;
+    });
+  return JSON.parse(result)
+}
+
+
+
+async function getNewsDetail(Url) {
+  try {
+    const response = await fetch(Url);
+    if (response.ok) {
+      const html = await response.text();
+      let parser = new DOMParser();
+      let doc = parser.parseFromString(html, 'text/html');
+      let objString = doc.querySelector(".module--detail script").textContent.trim().replace(/^var __DetailProp__ = /, "").replace(/;$/,"")
+      let jsonvar = stringJsObjToJson(objString)
+      let newsDetail = { "baseUrl": "https://www3.nhk.or.jp/news/" }
+      if (jsonvar.video && jsonvar.video !== "") {
+        newsDetail.video = jsonvar.video.replace(/^\/news\//, "")
+      } else if (jsonvar.img && jsonvar.img !== "") {
+        newsDetail.img = jsonvar.img.replace(/^\/news\//, "")
+      }
+      if (jsonvar.caption && jsonvar.caption !== "") {
+        newsDetail.caption = jsonvar.caption
+      }
+      if (jsonvar.title && jsonvar.title !== "") {
+        newsDetail.title = jsonvar.title
+      }
+      if (jsonvar.summary && jsonvar.summary !== "") {
+        newsDetail.summary = jsonvar.summary
+      }
+      if (jsonvar.more && jsonvar.more !== "") {
+        newsDetail.more = jsonvar.more
+      }
+      if (jsonvar.body) {
+        newsDetail.body = []
+        for (let i = 0; i < jsonvar.body.length; i++) {
+          let item = jsonvar.body[i];
+          if (item.detailType == "1" && (item.linkurl == "" || !item.linkurl)) {
+            let newItem = {};
+            if (item.title && item.title !== "") {
+              newItem.title = item.title;
+            }
+            if (item.video && item.video !== "") {
+              newItem.video = item.video.replace(/^\/news\//, "");
+            } else if (item.img && item.img !== "") {
+              newItem.img = item.img.replace(/^\/news\//, "");
+            }
+            if (item.caption && item.caption !== "") {
+              newItem.caption = item.caption;
+            }
+            if (item.text && item.text !== "") {
+              newItem.text = item.text;
+            }
+            newsDetail.body.push(newItem);
+          }
+        }
+      }
+      return { newsDetail };
+    } else {
+      return { news: "Failed to get news detail" };
+      //console.error('Failed to fetch HTML:', response.status);
+    }
+  } catch (error) {
+    return { news: "Failed to get news detail" };
+    //console.error('Error fetching HTML:', error);
   }
 }
